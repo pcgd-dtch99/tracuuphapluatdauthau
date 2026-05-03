98f0e4d3b21ccad6de7796749e9423635478ce84
pcgd-dtch99 <ivandiana0203@gmail.com>
2026-05-03 22:25:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/tracuuphapluatdauthau/',
     build: {
       assetsInlineLimit: 1048576, // 1MB limit to ensure the 374KB image is inlined
     },
